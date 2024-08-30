91db12b76370ec5acaf55216b4ea0209c7619566
kostasp31 <kostaspetrakis2003@gmail.com>
2024-08-30 13:01:13 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,81 @@
 import { useState, useEffect } from 'react'
 import loginS from './services/login'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link,
+  useParams, useNavigate,
+  Navigate
+} from 'react-router-dom'
 
-const Login = ({ userName, handleNameChange, password, handlePasswordChange, loginUser, showLogin, setShowLogin }) => {
-  if (showLogin) {
-    return (
-      <div>
-        <h2>Sign In</h2>
+const Login = ({ userName, handleNameChange, password, handlePasswordChange, loginUser, msg }) => {
+  return (
+    <div className='page'>
+      <a href='/' ><img src='../name.png' className='logo' /></a>
+      <Notification message={msg} />
+      <div className='forms' >
+        <h2 className='blk'>Sign In</h2>
         <form onSubmit={loginUser}>
           <div>
-            Username: <input
+            Email: <input
               value={userName}
-              onChange={handleNameChange}
+              onChange={handleNameChange} required
             />
           </div>
           <div>
             Password: <input
               value={password}
               type='password'
-              onChange={handlePasswordChange}
+              onChange={handlePasswordChange} required
             />
           </div>
           <div>
             <button type="submit">login</button>
-            <button className='button3' onClick={() => setShowLogin(!showLogin)} title="Close Form">&times;</button>
           </div>
         </form>
       </div>
-    )
-  }
-  else {
-    return null
-  }
+    </div>
+  )
 }
 
-const SignUp = ({ userName, handleNameChange, password, handlePasswordChange, loginUser, showSignup }) => {
-  if (showSignup) {
-    return (
-      <div style={{flow: 'right'}}>
+const SignUp = ({ userName, handleNameChange, password, handlePasswordChange, loginUser }) => {
+  return (
+    <div>
+      <a href='/' ><img src='../name.png' className='logo' /></a>
+      <div className='forms'>
         <h2>Sign Up</h2>
         <form onSubmit={loginUser}>
           <div>
-            Username: <input
+            First name: <input
               value={userName}
               onChange={handleNameChange}
             />
           </div>
           <div>
+            Last name: <input
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div>
+            Email: <input
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div>
+            Phone number: <input
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div>
             Password: <input
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div>
+            Confirm password: <input
               value={password}
               onChange={handlePasswordChange}
             />
@@ -56,14 +85,11 @@ const SignUp = ({ userName, handleNameChange, password, handlePasswordChange, lo
           </div>
         </form>
       </div>
-    )
-  }
-  else {
-    return null
-  }
+    </div>
+  )
 }
 
-const Notification = ({message}) => {
+const Notification = ({ message }) => {
   if (message === null)
     return null
   else {
@@ -75,16 +101,67 @@ const Notification = ({message}) => {
   }
 }
 
+const Welcome = () => {
+  return (
+    <>
+      <div>
+        <Link className='button' to="/">welcome</Link>
+        <Link className='button' to="/login" style={{ float: 'right' }} >Login</Link>
+        <Link className='button' to="/register" style={{ float: 'right' }}>Join Now</Link>
+        <Link className='button' to="/home">home</Link>
+      </div>
+      {/* <img src='../name.png' /> */}
+      <div>
+        <button className='button1' >Sign in</button>
+        <button className='button2' >Join now</button>
+      </div>
+    </>
+  )
+}
+
+const Home = ({ user, setUser }) => {
+  const navigate = useNavigate()
+  const logout = () => {
+    setUser(null)
+    window.localStorage.clear()
+    navigate('/')
+  }
+
+  return (
+    <>
+      <div>
+        <Link className='button' to="/">welcome</Link>
+        <button className='button' style={{ float: 'right', backgroundColor: '#ff1a1a' }} onClick={logout} >Logout</button>
+        <Link className='button' to="/home">home</Link>
+      </div>
+      {/* <img src='../name.png' /> */}
+      <div>
+        Welcome, {user.firstName} {user.lastName}!
+      </div>
+    </>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      This is a summer project for tedi
+      {/* <p>Kostas Petrakis & Filippos Papadimitriou, copyright 2024. All rights reserved &copy;</p> */}
+    </div>
+  )
+}
+
 const App = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
   const [newUserName, setNewUserName] = useState('')
   const [newPwassword, setNewPassword] = useState('')
-  const [showLogin, setShowLogin] = useState(false)
-  const [showSignup, setShowSignup] = useState(false)
   const [errorMessage, setErrorMessage] = useState(null)
-  
+
   const [user, setUser] = useState(null)
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     const loggedInUser = window.localStorage.getItem('loggedInUser')
@@ -124,7 +201,9 @@ const App = () => {
         }
       )
       setUser(user)
-      window.localStorage.setItem('loggedInUser', JSON.stringify(user)) 
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user))
+      navigate('/home')
+
     } catch (exception) {
       setErrorMessage(exception.response.data.error)
       setTimeout(() => {
@@ -143,52 +222,34 @@ const App = () => {
     return
   }
 
-  if (user === null) {
-    return (
-      <>
-        <img src='../name.png' />
-        <Notification message={errorMessage} />
-        <div>
-          {/* <ul>
-            <li> */}
-              <button className='button1' onClick={() => { if (!showLogin) setShowLogin(!showLogin) }}>Sign in</button>
-            {/* </li>
-            <li> */}
-              <button className='button2' onClick={() => { setShowSignup(!showSignup) }}>Join now</button>
-            {/* </li>
-          </ul> */}
+  return (
+    <div className='all'>
+      <Routes>
+        <Route path="/" element={!user ? <Welcome /> : <Navigate replace to="/home" />} />
+        <Route path="/login" element={
           <Login
             userName={email}
             password={password}
             handleNameChange={handleNameChange}
             handlePasswordChange={handlePasswordChange}
             loginUser={loginUser}
-            showLogin={showLogin}
-            setShowLogin={setShowLogin}
+            msg={errorMessage}
           />
+        } />
+        <Route path="/register" element={
           <SignUp
             userName={newUserName}
             password={newPwassword}
-            handleNameChange={handleNewNameChange} 
+            handleNameChange={handleNewNameChange}
             handlePasswordChange={handleNewPasswordChange}
             loginUser={signUpUser}
-            showSignup={showSignup}
           />
-        </div>
-      </>
-    )
-  }
-
-  return (
-      <>
-        {/* <h1>Linkedout</h1> */}
-        <img src='../name.png' style={{'float': 'center'}} />
-        <div>
-          {user.firstName} is logged in
-          <button onClick={() => {window.localStorage.clear(); setUser(null)}}>Logout</button>
-        </div>
-      </>
-    )
-  }
+        } />
+        <Route path="/home" element={user ? <Home user={user} setUser={setUser} /> : <Navigate replace to="/login" />} />
+      </Routes>
+      {/* <Footer /> */}
+    </div>
+  )
+}
 
 export default App
