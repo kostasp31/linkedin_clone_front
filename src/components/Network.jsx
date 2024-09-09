d83edfe408c11862ed50810226d16243747d997a
Filippos03 <philpap140@gmail.com>
2024-09-09 11:26:48 +0300

    network

--- src/components/Network.jsx
+++ src/components/Network.jsx
@@ -9,32 +9,73 @@ import {
     useParams, useNavigate,
     Navigate
 } from 'react-router-dom'
-import data from '../services/data'
 
-const AllConnected = () => { }
+const SearchResult = ({result}) => {
+    const navigate = useNavigate()
+    return(
+        <div>
+            <hr />
+            {result.fullName}
+            <button className='savebtn' onClick={() => {navigate(`/profile/${result.id}`)}}>Visit Profile</button>
+        </div>
+    )
+}
 
+const OneConnected = ({pr}) => {
+    const [persn, setPersn] = useState('')
+    const [prData, setPrData] = useState('')
+    useEffect(() => {
+        const fun = async () => {
+            const person = await userS.userInfo(pr)
+            setPersn(person)
+            // console.log(persn)
 
-// const OneConnected = () => {
-//     const navigate = useNavigate()
-//     const [persn, setPersn] = useState(null)
-//     useEffect(() => {
-//         const fun = async () => {
-//             const person = await userS.userInfo(usrData)
-//             setPersn(person)
-//         }
-//         fun()
-//     }, [])
+            const personData = await dataS.userData(person.userData.toString())
+            setPrData(personData)
+            // console.log(prData)
+        }
+        fun()
+    }, [])
 
-//     if (persn) {
-//         return (
-//             <div>
-//                 <div>
-//                     <p>{persn.firstName} {persn.lastName} {usrData.ad}</p>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
+    const printPfP = () => {
+        if (pr.pfp===null){
+            return ('no pfp')
+        } else {
+            return (<img style={{float:'right', width:'10%'}} src={persn.pfp} />)
+        }
+    }
+
+    const printWorks= () => {
+        if (prData.works===null){
+            return ('...')
+        } else {
+            return (prData.works)
+        }
+    }
+
+    const printPos = () => {
+        if (prData.position===null){
+            return('...')
+        } else {
+            return (prData.position)
+        }
+    }
+
+    const navigate = useNavigate()
+
+    if (persn) {
+        return (
+            <div>
+                <hr />
+                <h2>{persn.firstName} {persn.lastName} {printPfP()} </h2>  
+                <h3>Works in: {printWorks()}</h3>
+                <h3>Current position: {printPos()}</h3>
+                <button className='savebtn' onClick={() => {navigate(`/profile/${pr}`)}}>Visit Profile</button>
+                <button className='cancelbtn'>Remove from Network</button>
+            </div>
+        )
+    }
+}
 
 
 const Network = ({user, setUser}) => {
@@ -47,6 +88,26 @@ const Network = ({user, setUser}) => {
         }
         fun()
     }, [])
+
+    const [newSearch, setNewSearch] = useState('')
+    const [searchList, setSearchList] = useState([])
+
+    const onChangeHandler = async (value) => {
+        setNewSearch(value)
+        const allUsers = await userS.getAllUserData(user.token)
+        let fullNames = []
+        for (let usr of allUsers){
+            fullNames.push({fullName: usr.firstName + ' ' + usr.lastName, id: usr.id})
+        }
+        let matches = []
+        for (let i = 0; i<fullNames.length; i++){
+            if (fullNames[i].fullName.toLowerCase().includes(newSearch.toLowerCase())){
+                matches.push(fullNames[i])
+            }
+        }
+        console.log(matches)
+        setSearchList(matches)
+    }
 
     const navigate = useNavigate()
     if (!user)
@@ -69,25 +130,43 @@ const Network = ({user, setUser}) => {
                 <Link className='button' to="/home/settings">Settings</Link>
                 <button className='button' style={{ float: 'right', backgroundColor: '#ff1a1a' }} onClick={logout} >Logout</button>
             </div>
-            {/* <img src='../name.png' /> */}
+
             <div>
                 {usrData && user ?
                     <div className='container_home'>
-                        <div className='column_left_home'>
-                            <h1 style={{ textAlign: 'center' }}>SEARCH</h1>
+                        <div className='column_left_home' style={{width: '30%'}}>
+                            <div className='usrInfoOuter' style={{ marginTop: '0'}}>
+                                <div className='usrInfoInner' style={{ width: '90%' }}>
+                                    <h1 style={{ textAlign: 'center' }}>SEARCH</h1>
+                                    <h2 style={{textAlign:'center'}}>Search users by name<br />
+                                        <input style={{width:'100%'}} value={newSearch} onChange={(event) => onChangeHandler(event.target.value)} />
+                                    </h2>
+                                    <ul>
+                                        {searchList.map((res) =>
+                                            <SearchResult result={res} key={res} />
+                                        )}
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                         <div className='column_right_home'>
-                            <h1 style={{ textAlign: 'center' }}>MY NETWORK</h1>
-                            {usrData.network.length ?
-                                <div>
-
+                            <div className='usrInfoOuter' style={{ marginTop: '0'}}>
+                                <div className='usrInfoInner' style={{ width: '100%' }}>
+                                    <h1 style={{ textAlign: 'center' }}>MY NETWORK</h1>
+                                    {usrData.network.length ?
+                                        <ul>
+                                            {usrData.network.map((person) =>
+                                                <OneConnected pr={person} key={person} />
+                                            )}
+                                        </ul>
+                                        :
+                                        <div>Network empty</div>
+                                    }
                                 </div>
-                                :
-                                <div>No friends :(</div>
-                            }
+                            </div>
                         </div>
                     </div>
-                    : ''}
+                : 'ERROR'}
             </div>
         </>
     )
